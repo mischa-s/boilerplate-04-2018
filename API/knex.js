8f54f2dff7a22d7338598d9357f9e6a9a444036c
Mischa Saunders <mischa.saunders@gmail.com>
2018-04-10 20:29:11 +1200

    log client and connection

--- API/knex.js
+++ API/knex.js
@@ -2,6 +2,7 @@ const knex = require('knex');
 
 module.exports = function (app) {
   const { client, connection } = app.get('postgres');
+  console.log(client, 'client', connection, 'connection')
   const db = knex({ client, connection });
 
   app.set('knexClient', db);
